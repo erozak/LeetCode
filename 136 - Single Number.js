34b74f8d69afb7bc1ae8c012b7fd12bed36d9762
Erozak <erozakrocwols@gmail.com>
2017-11-23 00:06:23 +0800

    Coding: 136, 693, 728.

---
+++ 136 - Single Number.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/single-number
+
+/*
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  const set = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      set.delete(nums[i]);
+    } else {
+      set.add(nums[i]);
+    }
+  }
+
+  return Number(Array.from(set).join(''));
+};
