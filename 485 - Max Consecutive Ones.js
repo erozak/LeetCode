283b7332925b060ef14578f7641ea09c99ac2f17
Erozak <erozakrocwols@gmail.com>
2017-11-25 15:41:12 +0800

    Add #485.

---
+++ 485 - Max Consecutive Ones.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/max-consecutive-ones/
+
+/*
+Given a binary array, find the maximum number of consecutive 1 s in this array.
+
+Example 1: Input: [1, 1, 0, 1, 1, 1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1 s.
+The maximum number of consecutive 1 s is 3.
+Note:
+
+  The input array will only contain 0 and 1.
+The length of input array is a positive integer and will not exceed 10, 000
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+  return nums
+    .join('')
+    .split(/0+/)
+    .reduce((acc, ones) => (
+      Math.max(acc, ones.length)
+    ), 0);
+};
